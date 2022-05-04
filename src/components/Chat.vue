2d20694bbabc2e239dd38425b0e24638b3fbe03f
Behzodjon <behzodjontuit1998@gmail.com>
2022-05-04 23:10:25 +0500

    feat: chat style

---
+++ src/components/Chat.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="fixed bottom-0 z-50">
+        <div
+            class="fixed top-0 bottom-0 right-0 flex flex-col max-h-screen bg-white w-[240px] border-l border-solid border-[#e7edf2]">
+            <div class="flex flex-col">
+                <div class="flex justify-between h-20 px-6 py-2  border-b border-solid border-[#e7edf2] items-center">
+                    <div class="relative inline-flex items-center space-x-2">
+                        <comments class="w-5 text-pink-500 h-7" />
+                        <div class="text-[1em] font-bold text-[#4f515b] quicksand">Messenger</div>
+                    </div>
+                    <div>
+                        <div>
+                            <ellipsis class="w-4 h-7" />
+                        </div>
+                    </div>
+                </div>
+                <div class="px-6 py-3">
+                    <TabGroup>
+                        <TabList class="flex space-x-3">
+                            <Tab v-slot="{ selected }" as="template">
+                                <button :class="[selected ? ' text-pink-500 border-none outline-none' : '']">
+                                    Friends
+                                </button>
+                            </Tab>
+                            <Tab v-slot="{ selected }" as="template"> <button
+                                    :class="[selected ? ' text-pink-500 border-none outline-none' : '']">
+                                    Groups
+                                </button></Tab>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>Content 1</TabPanel>
+                            <TabPanel>Content 2</TabPanel>
+                        </TabPanels>
+                    </TabGroup>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import comments from '../assets/images/icons/comments.svg'
+import ellipsis from '../assets/images/icons/ellipsis.svg'
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+
+
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -44,81 +44,92 @@
             <div class="flex flex-col items-center flex-1 w-full min-h-0 sidebar">
                 <perfect-scrollbar class="w-full pb-20">
 
-                <!-- panel dark -->
-                <div class="w-full px-4 pt-10 pb-4 bg-linear bg-dark-100">
-                    <div class="flex items-center justify-center flex-shrink-0 w-full">
-                        <img class="max-h-[105px]  max-w-[80%]" src="../assets/images/logo.svg" alt="Beehive" />
+                    <!-- panel dark -->
+                    <div class="w-full px-4 pt-10 pb-4 bg-linear bg-dark-100">
+                        <div class="flex items-center justify-center flex-shrink-0 w-full">
+                            <img class="max-h-[105px]  max-w-[80%]" src="../assets/images/logo.svg" alt="Beehive" />
+                        </div>
+                        <!-- user card -->
+                        <UserCard />
                     </div>
-                    <!-- user card -->
-                    <UserCard />
-                </div>
 
-                <!-- panel light -->
-                <div class="border-r border-solid border-[#e7edf2] pb-4  px-4 w-full bg-[#f8f9fb] py-[90px]">
-                    <PanelMenu/>
-                </div>
+                    <!-- panel light -->
+                    <div class="border-r border-solid border-[#e7edf2] pb-4  px-4 w-full bg-[#f8f9fb] py-[90px]">
+                        <PanelMenu />
+                    </div>
                 </perfect-scrollbar>
             </div>
         </div>
 
         <!-- content block -->
         <div class="md:pl-[22%] flex flex-col">
-            <div class="sticky top-0 z-10 flex flex-shrink-0 h-16 bg-white shadow">
+            <div class="sticky top-0 z-10 flex flex-shrink-0 h-20 bg-white shadow">
                 <button type="button"
                     class="px-4 text-gray-500 border-r border-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
                     @click="sidebarOpen = true">
                     <span class="sr-only">Open sidebar</span>
                     <MenuAlt2Icon class="w-6 h-6" aria-hidden="true" />
                 </button>
-                <div class="flex justify-between flex-1 px-4">
-                    <div class="flex flex-1">
+                <div class="flex justify-between flex-1 pl-[25px] pr-[95px]">
+                    <div class="flex flex-1 px-4">
                         <form class="flex w-full md:ml-0" action="#" method="GET">
                             <label for="search-field" class="sr-only">Search</label>
                             <div class="relative w-full text-gray-400 focus-within:text-gray-600">
                                 <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                                    <SearchIcon class="w-5 h-5" aria-hidden="true" />
+                                    <search class="w-4 h-4 text-[#626c72]" />
                                 </div>
                                 <input id="search-field"
-                                    class="block w-full h-full py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 border-transparent focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                                    placeholder="Search" type="search" name="search" />
+                                    class="block w-full h-full py-2 pl-6 pr-3 text-gray-900 placeholder-gray-500 border-transparent focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
+                                    placeholder="Search..." type="search" name="search" />
                             </div>
                         </form>
                     </div>
                     <div class="flex items-center ml-4 md:ml-6">
-                        <button type="button"
-                            class="p-1 text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            <span class="sr-only">View notifications</span>
-                            <BellIcon class="w-6 h-6" aria-hidden="true" />
-                        </button>
+                        <ul class="flex">
+                            <li class="relative">
+                                <div class="relative block px-2">
+                                    <div
+                                        class="bg-[#f8f9fb] text-base rounded-[50%] w-8 h-8 flex justify-center items-center">
+                                        <UserPlus class="w-4 h-4 text-[#626c72] " />
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="relative">
+                                <div class="relative block px-2">
+                                    <div
+                                        class="bg-[#f8f9fb] text-base rounded-[50%] w-8 h-8 flex justify-center items-center">
+                                        <bell class="w-4 h-4 text-[#626c72] " />
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="relative">
+                                <div class="relative block px-2">
+                                    <div
+                                        class="bg-[#f8f9fb] text-base rounded-[50%] w-8 h-8 flex justify-center items-center">
+                                        <EnvelopeOpen class="w-4 h-4 text-[#626c72] " />
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="relative">
+                                <div class="relative block px-2">
+                                    <div
+                                        class="bg-[#f8f9fb] text-base rounded-[50%] w-8 h-8 flex justify-center items-center">
+                                        <ShoppingBag class="w-4 h-4 text-[#626c72] " />
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="relative">
+                                <div class="relative flex items-center pl-2 space-x-1">
+                                    <div class="flex items-center justify-center w-8 h-8 ">
+                                        <img class="rounded-[50%] w-full h-full object-cover"
+                                            src="../assets/images/john-doe.jpg" alt="avatar">
+                                    </div>
+                                    <span class="block text-[#29292d]">@ user</span>
 
-                        <!-- Profile dropdown -->
-                        <Menu as="div" class="relative ml-3">
-                            <div>
-                                <MenuButton
-                                    class="flex items-center max-w-xs text-sm bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                    <span class="sr-only">Open user menu</span>
-                                    <img class="w-8 h-8 rounded-full"
-                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                        alt="" />
-                                </MenuButton>
-                            </div>
-                            <transition enter-active-class="transition duration-100 ease-out"
-                                enter-from-class="transform scale-95 opacity-0"
-                                enter-to-class="transform scale-100 opacity-100"
-                                leave-active-class="transition duration-75 ease-in"
-                                leave-from-class="transform scale-100 opacity-100"
-                                leave-to-class="transform scale-95 opacity-0">
-                                <MenuItems
-                                    class="absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                                    <a :href="item.href"
-                                        :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{
-                                                item.name
-                                        }}</a>
-                                    </MenuItem>
-                                </MenuItems>
-                            </transition>
-                        </Menu>
+                                </div>
+                            </li>
+                        </ul>
+
                     </div>
                 </div>
             </div>
@@ -138,6 +149,7 @@
                 </div>
             </main>
         </div>
+        <Chat/>
     </div>
 </template>
 
@@ -146,21 +158,21 @@ import { ref } from 'vue'
 import {
     Dialog,
     DialogOverlay,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems,
     TransitionChild,
     TransitionRoot,
 } from '@headlessui/vue'
 import {
-    BellIcon,
     MenuAlt2Icon,
     XIcon,
 } from '@heroicons/vue/outline'
-import { SearchIcon } from '@heroicons/vue/solid'
 import UserCard from '../components/UserCard.vue'
 import PanelMenu from '../components/PanelMenu.vue'
+import search from '../assets/images/icons/search.svg'
+import UserPlus from '../assets/images/icons/user-plus.svg'
+import bell from '../assets/images/icons/bell.svg'
+import EnvelopeOpen from '../assets/images/icons/envelope-open.svg'
+import ShoppingBag from '../assets/images/icons/shopping-bag.svg'
+import Chat from '../components/Chat.vue'
 
 
 
